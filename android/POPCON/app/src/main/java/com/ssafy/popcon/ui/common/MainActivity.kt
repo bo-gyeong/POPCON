874f56ac89e582e605f95df5a67e1503488427ae
장보경 <jjjjyj4870@naver.com>
2023-01-11 16:53:22 +0900

    @jjjjyj4870 전화, 갤러리, 이미지 permission

--- android/POPCON/app/src/main/java/com/ssafy/popcon/ui/common/MainActivity.kt
+++ android/POPCON/app/src/main/java/com/ssafy/popcon/ui/common/MainActivity.kt
@@ -1,15 +1,26 @@
 package com.ssafy.popcon.ui.common
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
 import com.ssafy.popcon.R
 import com.ssafy.popcon.databinding.ActivityMainBinding
+import com.ssafy.popcon.util.CheckPermission
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+
+    private lateinit var checkPermission: CheckPermission
+
+    val PERMISSION_REQUEST_CODE = 8
+    val CALL_REQUEST = 100
+    val POSITION_REQUEST = 500
+    val GALLERY_REQUEST = 1000
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,11 +39,57 @@ class MainActivity : AppCompatActivity() {
                 binding.tabLayoutBottomNavigation.selectedItemId = item.itemId
             }
         }
+
+        checkPermissions()
     }
 
-    private fun checkPermissions(){
-        // 위치, 갤러리, 전화
+    private val runtimePermissions = arrayOf(
+        Manifest.permission.ACCESS_FINE_LOCATION,
+        Manifest.permission.ACCESS_COARSE_LOCATION,
+        Manifest.permission.CALL_PHONE,
+        Manifest.permission.READ_EXTERNAL_STORAGE,
+        Manifest.permission.WRITE_EXTERNAL_STORAGE,
 
+    )
+
+    // 위치, 갤러리, 전화 권한
+    private fun checkPermissions(){
+        checkPermission = CheckPermission(this)
+
+        if (!checkPermission.runtimeCheckPermission(this, *runtimePermissions)) {
+            ActivityCompat.requestPermissions(this, runtimePermissions, PERMISSION_REQUEST_CODE)
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when(requestCode){
+            CALL_REQUEST -> {
+                if (grantResults.size > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    //권한 승인
+                } else{
+                    checkPermission.requestPermission()
+                }
+            }
+            POSITION_REQUEST -> {
+                if (grantResults.size > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    //권한 승인
+                } else{
+                    checkPermission.requestPermission()
+                }
+            }
+            GALLERY_REQUEST -> {
+                if (grantResults.size > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    //권한 승인
+                } else{
+                    checkPermission.requestPermission()
+                }
+            }
+        }
     }
 
     fun hideBottomNav(state: Boolean) {
