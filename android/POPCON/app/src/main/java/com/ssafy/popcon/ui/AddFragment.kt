95336854c9ebc79b1b24f13f8bb35bc67c189a00
장보경 <jjjjyj4870@naver.com>
2023-01-12 10:05:15 +0900

    @jjjjyj4870 add구현중

--- android/POPCON/app/src/main/java/com/ssafy/popcon/ui/AddFragment.kt
+++ android/POPCON/app/src/main/java/com/ssafy/popcon/ui/AddFragment.kt
@@ -1,14 +1,30 @@
 package com.ssafy.popcon.ui
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.ssafy.popcon.R
 import com.ssafy.popcon.databinding.FragmentAddBinding
+import com.ssafy.popcon.ui.common.MainActivity
 
 class AddFragment : Fragment() {
     private lateinit var binding: FragmentAddBinding
+
+    lateinit var mainActivity: MainActivity
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        mainActivity = context as MainActivity
+    }
+
+    override fun onStart() {
+        super.onStart()
+        mainActivity.hideBottomNav(true)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
