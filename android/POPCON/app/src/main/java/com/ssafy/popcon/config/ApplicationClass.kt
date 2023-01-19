a0c74c5d5cf7feada6ec4939c9d5afb1fc0964aa
unme97 <unme97@naver.com>
2023-01-19 15:50:36 +0900

    S08P12D201-48 @unme97 feat : key, url 수정

--- android/POPCON/app/src/main/java/com/ssafy/popcon/config/ApplicationClass.kt
+++ android/POPCON/app/src/main/java/com/ssafy/popcon/config/ApplicationClass.kt
@@ -2,6 +2,7 @@ package com.ssafy.popcon.config
 
 import android.Manifest
 import android.app.Application
+import com.ssafy.popcon.BuildConfig
 import com.ssafy.popcon.util.SharedPreferencesUtil
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
@@ -10,8 +11,8 @@ import retrofit2.converter.gson.GsonConverterFactory
 import java.util.concurrent.TimeUnit
 
 class ApplicationClass : Application() {
-    companion object{
-        const val SERVER_URL = "http://192.168.100.136:8080/"
+    companion object {
+        const val SERVER_URL = BuildConfig.BASE_URL
 
         lateinit var sharedPreferencesUtil: SharedPreferencesUtil
         lateinit var retrofit: Retrofit
@@ -21,7 +22,7 @@ class ApplicationClass : Application() {
             Manifest.permission.ACCESS_FINE_LOCATION,
         )
 
-        fun makeRetrofit(url : String) : Retrofit {
+        fun makeRetrofit(url: String): Retrofit {
             val okHttpClient = OkHttpClient.Builder()
                 .readTimeout(5000, TimeUnit.MILLISECONDS)
                 .connectTimeout(5000, TimeUnit.MILLISECONDS)
