2c640db5c004a86193e52692c78a06e7187186dc
unme97 <unme97@naver.com>
2023-01-25 10:22:28 +0900

    S08P12D201-50 @unme97 map dto dir change

---
+++ android/POPCON/app/src/main/java/com/ssafy/popcon/dto/Map.kt
@@ -0,0 +1,11 @@
+package com.ssafy.popcon.dto
+
+data class MapBrandLogo(
+//    phone, placeName, xPos, yPos, brand
+    val brandName: String,
+    val itemName: String,
+    val xPos: Double,
+    val yPos: Double,
+    val brandLogo: String,
+    val phone: String
+)
