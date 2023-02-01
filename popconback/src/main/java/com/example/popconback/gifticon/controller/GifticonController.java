da6dde2d9be06dba67c3360714799f928825e9f3
조재완 <johjaewan@naver.com>
2023-02-01 14:11:43 +0900

    @johjaewan feat : api 브랜드 호출

--- popconback/src/main/java/com/example/popconback/gifticon/controller/GifticonController.java
+++ popconback/src/main/java/com/example/popconback/gifticon/controller/GifticonController.java
@@ -92,7 +92,7 @@ public class GifticonController {
 
 
     @ApiOperation(value = "기프티콘 정렬", notes = "기프티콘 브랜드별 정렬", httpMethod = "GET") // get 으로 수정
-    @GetMapping("/brand") //기프티콘 브랜드별 정렬 // 사용한거 표시 제외하고 보낼지 말지 고민
+    @PostMapping("/brand") //기프티콘 브랜드별 정렬 // 사용한거 표시 제외하고 보낼지 말지 고민
     public ResponseEntity<List<ResponseListGifticonUserDto>> SortGifticon (@RequestBody SortGifticonDto sortGifticonDto){
             return ResponseEntity.ok(gifticonService.sortGifticon(sortGifticonDto));
    }
