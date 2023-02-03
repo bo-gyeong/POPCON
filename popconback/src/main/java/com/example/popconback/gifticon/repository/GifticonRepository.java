ddd95edd49208911045581cb3e7b625daa90cbf6
조재완 <johjaewan@naver.com>
2023-02-03 10:58:46 +0900

    @johjaewan feat : api 브랜드 정렬 사용완료 미표시

--- popconback/src/main/java/com/example/popconback/gifticon/repository/GifticonRepository.java
+++ popconback/src/main/java/com/example/popconback/gifticon/repository/GifticonRepository.java
@@ -15,7 +15,7 @@ import java.util.Map;
 
 public interface GifticonRepository extends JpaRepository<Gifticon, String> {
     List<Gifticon> findByUser_HashAndState(int hash, int state, Sort sort);
-    List<Gifticon> findByUser_HashAndBrand_BrandName(int hash, String brand_name);
+    List<Gifticon> findByUser_HashAndBrand_BrandNameAndState(int hash, String brand_name,int state, Sort sort);
     List<Gifticon>  findByUser_HashAndDueLessThanEqualAndState(int hash, Date date, int state);
     List<Gifticon> findByDueAndState(Date date, int state);
 
--- popconback/src/main/java/com/example/popconback/gifticon/service/GifticonService.java
+++ popconback/src/main/java/com/example/popconback/gifticon/service/GifticonService.java
@@ -58,7 +58,8 @@ public class GifticonService {
 
         List<ResponseListGifticonUserDto> rlist = new ArrayList<>();
 
-        for (int i = 0; i < 2; i++){
+        for (int i = 0; i < 3; i++){
+            if(i == 1){continue;}
             List<Gifticon>list = gifticonRepository.findByUser_HashAndState(hash,i, Sort.by(asc("due")));
             for (Gifticon gifticon:list) {
                 ResponseListGifticonUserDto rgifticon = new ResponseListGifticonUserDto();
@@ -198,31 +199,35 @@ public class GifticonService {
             return rlist;
         }
 
-        List <Gifticon>list = gifticonRepository.findByUser_HashAndBrand_BrandName(hash,sortGifticonDto.getBrandName());
-        for (Gifticon gifticon: list
-             ) {
-            ResponseListGifticonUserDto rgifticon = new ResponseListGifticonUserDto();
-            BeanUtils.copyProperties(gifticon, rgifticon);
-
-            BrandForRLGUDto rbrand = new BrandForRLGUDto();
-            BeanUtils.copyProperties(gifticon.getBrand(),rbrand);
-            rgifticon.setBrand(rbrand);
-
-            List<GifticonFiles>gflist = gifticonFilesRepository.findByGifticon_BarcodeNum(gifticon.getBarcodeNum());//사진들도 따로 복사
-            for (GifticonFiles gifticonfile: gflist
+        for (int i = 0; i < 3; i++){
+            if(i == 1){continue;}
+            List <Gifticon>list = gifticonRepository.findByUser_HashAndBrand_BrandNameAndState(hash,sortGifticonDto.getBrandName(),i,Sort.by(asc("due")));
+            for (Gifticon gifticon: list
             ) {
-                if (gifticonfile.getImageType() == 0) {// 0: 바코드
-                    rgifticon.setBarcode_filepath(gifticonfile.getFilePath());
+                ResponseListGifticonUserDto rgifticon = new ResponseListGifticonUserDto();
+                BeanUtils.copyProperties(gifticon, rgifticon);
+
+                BrandForRLGUDto rbrand = new BrandForRLGUDto();
+                BeanUtils.copyProperties(gifticon.getBrand(),rbrand);
+                rgifticon.setBrand(rbrand);
+
+                List<GifticonFiles>gflist = gifticonFilesRepository.findByGifticon_BarcodeNum(gifticon.getBarcodeNum());//사진들도 따로 복사
+                for (GifticonFiles gifticonfile: gflist
+                ) {
+                    if (gifticonfile.getImageType() == 0) {// 0: 바코드
+                        rgifticon.setBarcode_filepath(gifticonfile.getFilePath());
+                    }
+                    if (gifticonfile.getImageType() == 1) {// 1: 상품
+                        rgifticon.setProduct_filepath(gifticonfile.getFilePath());
+                    }
+                    if (gifticonfile.getImageType() == 2) {// 2: 원본
+                        rgifticon.setOrigin_filepath(gifticonfile.getFilePath());
+                    }
                 }
-                if (gifticonfile.getImageType() == 1) {// 1: 상품
-                    rgifticon.setProduct_filepath(gifticonfile.getFilePath());
-                }
-                if (gifticonfile.getImageType() == 2) {// 2: 원본
-                    rgifticon.setOrigin_filepath(gifticonfile.getFilePath());
-                }
+                rlist.add(rgifticon);
             }
-            rlist.add(rgifticon);
         }
+
         return rlist;
     }
 
