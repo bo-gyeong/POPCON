4a20faff9b86fe94c32c0767fd03cdd39c4a4505
장보경 <jjjjyj4870@naver.com>
2023-02-14 15:03:12 +0900

    S08P12D201-420 @jjjjyj4870 feat : 워치 로그인 화면

--- android/POPCON/wear/src/main/java/com/ssafy/popcon/ui/login/LoginActivity.kt
+++ android/POPCON/wear/src/main/java/com/ssafy/popcon/ui/login/LoginActivity.kt
@@ -1,15 +1,12 @@
 package com.ssafy.popcon.ui.login
 
-import android.annotation.SuppressLint
-import android.content.Context
 import android.content.Intent
 import android.graphics.drawable.Drawable
 import android.net.Uri
 import android.os.Bundle
-import android.util.AttributeSet
 import android.util.Log
 import android.view.View
-import androidx.activity.viewModels
+import android.view.animation.AnimationUtils
 import androidx.appcompat.app.AppCompatActivity
 import androidx.wear.ambient.AmbientModeSupport
 import com.bumptech.glide.Glide
@@ -17,34 +14,17 @@ import com.bumptech.glide.load.resource.gif.GifDrawable
 import com.bumptech.glide.request.target.DrawableImageViewTarget
 import com.bumptech.glide.request.transition.Transition
 import com.google.android.gms.wearable.*
-import com.kakao.sdk.auth.model.OAuthToken
-import com.kakao.sdk.common.model.ClientError
-import com.kakao.sdk.common.model.ClientErrorCause
-import com.kakao.sdk.common.util.Utility
-import com.kakao.sdk.user.UserApiClient
-import com.navercorp.nid.NaverIdLoginSDK
-import com.navercorp.nid.oauth.NidOAuthLogin
-import com.navercorp.nid.oauth.OAuthLoginCallback
-import com.navercorp.nid.profile.NidProfileCallback
-import com.navercorp.nid.profile.data.NidProfileResponse
 import com.ssafy.popcon.R
 import com.ssafy.popcon.config.WearApplicationClass
 import com.ssafy.popcon.databinding.ActivityLoginBinding
-import com.ssafy.popcon.dto.TokenResponse
 import com.ssafy.popcon.dto.User
-import com.ssafy.popcon.repository.user.WearRemoteDataSource
-import com.ssafy.popcon.repository.user.WearRepository
 import com.ssafy.popcon.ui.map.DonateActivity
 import com.ssafy.popcon.util.SharedPreferencesUtil
-import com.ssafy.popcon.util.WearRetrofitUtil
-import com.ssafy.popcon.viewmodel.WearViewModel
-import com.ssafy.popcon.viewmodel.ViewModelFactoryWear
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 import java.nio.charset.StandardCharsets
-import java.util.*
 
 private const val TAG = "LoginFragment_싸피"
 
@@ -62,11 +42,6 @@ class LoginActivity : AppCompatActivity(), AmbientModeSupport.AmbientCallbackPro
         setContentView(binding.root)
     }
 
-    override fun onCreateView(name: String, context: Context, attrs: AttributeSet): View? {
-        //floatLogo()
-        return super.onCreateView(name, context, attrs)
-    }
-
     override fun onResume() {
         super.onResume()
         Wearable.getDataClient(this).addListener(this)
@@ -75,7 +50,7 @@ class LoginActivity : AppCompatActivity(), AmbientModeSupport.AmbientCallbackPro
             .addListener(this, Uri.parse("wear://"), CapabilityClient.FILTER_REACHABLE)
 
         supportActionBar!!.hide()
-        //SharedPreferencesUtil(this@LoginActivity).deleteUser()
+        floatLogo()
     }
 
     override fun onPause() {
@@ -119,8 +94,7 @@ class LoginActivity : AppCompatActivity(), AmbientModeSupport.AmbientCallbackPro
             val intent = Intent(this, DonateActivity::class.java)
             startActivity(intent)
 
-            //finish()
-
+            finish()
         } catch (e: Exception) {
             Log.d(TAG_MESSAGE_RECEIVED, "Handled in onMessageReceived")
             e.printStackTrace()
@@ -139,11 +113,16 @@ class LoginActivity : AppCompatActivity(), AmbientModeSupport.AmbientCallbackPro
                     transition: Transition<in Drawable>?
                 ) {
                     if (resource is GifDrawable) {
-                        (resource as GifDrawable).setLoopCount(1)
-                        binding.tvLoginInfo.visibility = View.VISIBLE
+                        resource.setLoopCount(1)
                     }
                     super.onResourceReady(resource, transition)
                 }
             })
+
+        val animationUp = AnimationUtils.loadAnimation(applicationContext, R.anim.login_logo_up)
+        binding.ivLogo.animation = animationUp
+
+        val animationDown = AnimationUtils.loadAnimation(applicationContext, R.anim.login_text_down)
+        binding.tvLoginInfo.animation = animationDown
     }
 }
